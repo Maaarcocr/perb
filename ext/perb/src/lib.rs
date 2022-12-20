a317695256440940d904fc2455a12dda4bca7c4c
Maaarcocr <marcoc.r@outlook.com>
2022-12-20 19:15:17 +0100

    use dynasm to produce assembly

--- ext/perb/src/lib.rs
+++ ext/perb/src/lib.rs
@@ -1,8 +1,24 @@
-use magnus::{define_module, function};
+use std::mem;
+
+use magnus::{define_module, function, rb_sys::FromRawValue};
+use dynasmrt::{dynasm, DynasmApi};
 
 fn wrapper() -> magnus::Value {
-    unsafe{
-    rb_sys::rb_yield(0)
+    let mut ops = dynasmrt::x64::Assembler::new().unwrap();
+    let asm_wrapper = ops.offset();
+    dynasm!(ops
+        ; xor rcx, rcx
+        ; mov rax, QWORD rb_sys::rb_yield as _
+        ; call rax
+        ; ret
+    );
+    let buf = ops.finalize().unwrap();
+
+    let asm_wrapper_fn: extern "win64" fn() -> rb_sys::Value =
+        unsafe { mem::transmute(buf.ptr(asm_wrapper)) };
+
+    unsafe {
+        magnus::Value::from_raw(asm_wrapper_fn())
     }
 }
 
--- test/test_perb.rb
+++ test/test_perb.rb
@@ -7,10 +7,45 @@ class TestPerb < Minitest::Test
     refute_nil ::Perb::VERSION
   end
 
-  def test_it_does_something_useful
+  def test_it_returns_120
     result = ::Perb.wrapper do
       120
     end
     assert_equal(result, 120)
+  end
+
+  def test_it_returns_hello_world
+    result = ::Perb.wrapper do
+      "Hello, World!"
+    end
+    assert_equal(result, "Hello, World!")
+  end
+
+  def test_it_returns_1_dot_20
+    result = ::Perb.wrapper do
+      1.20
+    end
+    assert_equal(result, 1.20)
+  end
+
+  def test_it_returns_true
+    result = ::Perb.wrapper do
+      true
+    end
+    assert_equal(result, true)
+  end
+
+  def test_it_returns_array
+    result = ::Perb.wrapper do
+      [1, 2, 3]
+    end
+    assert_equal(result, [1, 2, 3])
+  end
+
+  def test_it_returns_hash
+    result = ::Perb.wrapper do
+      { a: 1, b: 2, c: 3 }
+    end
+    assert_equal(result, { a: 1, b: 2, c: 3 })
   end
 end
