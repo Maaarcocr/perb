4ebc81a46559439b2e05e0aa13d32954f06753da
Maaarcocr <marcoc.r@outlook.com>
2023-01-05 01:17:54 +0100

    use one assembler per wrapper

--- ext/perb/src/lib.rs
+++ ext/perb/src/lib.rs
@@ -1,14 +1,10 @@
-use std::{io::Write, mem, sync::RwLock};
+use std::{io::Write, mem};
 
 use dynasmrt::{dynasm, AssemblyOffset, DynasmApi};
 use magnus::{define_module, function, rb_sys::FromRawValue};
-use once_cell::sync::Lazy;
-
-static ASSEMBLER: Lazy<RwLock<dynasmrt::x64::Assembler>> =
-    Lazy::new(|| RwLock::new(dynasmrt::x64::Assembler::new().unwrap()));
 
 fn build_wrapper(fn_name: String) -> usize {
-    let mut ops = ASSEMBLER.write().unwrap();
+    let mut ops = dynasmrt::x64::Assembler::new().unwrap();
     let asm_wrapper = ops.offset();
 
     dynasm!(ops
@@ -25,8 +21,7 @@ fn build_wrapper(fn_name: String) -> usize {
 
     ops.commit().unwrap();
 
-    let executor = ops.reader();
-    let buf = executor.lock();
+    let buf = ops.finalize().unwrap();
 
     let file_name = format!("/tmp/perf-{}.map", std::process::id());
     let file = std::fs::OpenOptions::new()
@@ -34,27 +29,21 @@ fn build_wrapper(fn_name: String) -> usize {
         .create(true)
         .open(file_name)
         .unwrap();
-
+    let fn_ptr = buf.ptr(asm_wrapper) as usize;
     let mut line_writer = std::io::LineWriter::new(file);
     let len = unsafe { buf.ptr(end_offset).offset_from(buf.ptr(asm_wrapper)) };
     let perf_map = format!(
         "{:x} {:x} {}\n",
-        buf.ptr(asm_wrapper) as usize,
+        fn_ptr,
         len,
         fn_name
     );
     line_writer.write_all(perf_map.as_bytes()).unwrap();
-
-    asm_wrapper.0
+    Box::leak(Box::new(buf));
+    fn_ptr
 }
 
 fn wrapper(fn_pointer: usize) -> magnus::Value {
-    let fn_pointer = ASSEMBLER
-        .read()
-        .unwrap()
-        .reader()
-        .lock()
-        .ptr(AssemblyOffset(fn_pointer));
     unsafe {
         let asm_wrapper_fn: extern "C" fn() -> rb_sys::Value = mem::transmute(fn_pointer);
         magnus::Value::from_raw(asm_wrapper_fn())
